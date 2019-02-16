3a6727dd5ad14dcc479bd02b0a5d93ef7bfa1ff8
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-02-16 12:06:48 +0100

    Add Password features

--- src/main/java/com/skoneczny/controllers/changePasswordController.java
+++ src/main/java/com/skoneczny/controllers/changePasswordController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.skoneczny.api.IPasswordService;
 import com.skoneczny.api.IUserService;
@@ -21,6 +22,11 @@ public class changePasswordController {
 	public String changePassword(Model model,Principal principal) {
 		String email = principal.getName();		
 		model.addAttribute("password", new Password());
+		return "views/changePasswordForm";
+	}
+	
+	@PostMapping("/changePassword")
+	public String changePassword(Principal principal) {
 		return "views/changePasswordForm";
 	}
 
