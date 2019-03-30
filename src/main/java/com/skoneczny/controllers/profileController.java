675cec5e59ba66d2d7f400637b7abdae1df383ed
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-03-30 13:51:54 +0100

    Dalej działania z profile data

--- src/main/java/com/skoneczny/controllers/profileController.java
+++ src/main/java/com/skoneczny/controllers/profileController.java
@@ -4,11 +4,9 @@ import java.security.Principal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +14,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.resource.HttpResource;
 
 import com.skoneczny.entites.Task;
 import com.skoneczny.entites.User;
@@ -48,6 +43,67 @@ public class profileController {
 			HttpSession session,
 			HttpServletRequest request,
 			Pageable pageable
+			) {
+		int clikedPage;
+		int currentPage = page.orElse(0/*1*/);
+		int pageSize = size.orElse(5);
+		
+		if(session.getAttribute("emailSession") != null) {
+			if(!session.getAttribute("emailSession").equals(email)) {
+				session.removeAttribute("currentPage");
+			}
+		}		
+		if(session.getAttribute("currentPage") != null) {
+			clikedPage = page.isPresent()? currentPage : (int) session.getAttribute("currentPage") ;
+			currentPage = clikedPage;
+		}else
+		{
+			clikedPage = currentPage;
+		}
+		Sort sortP = pageable.getSort();	
+		
+        String sortParam = sort.orElse("startDate");
+        String year = Integer.toString(LocalDate.now().getYear());		
+		if(email.isEmpty()) email = principal.getName();
+		User user = userService.findOne(email);
+//		String allYear = "All";
+		model.addAttribute("years", taskService.getAllYeas(user));
+		model.addAttribute("email",user.getEmail());
+//		model.addAttribute("allYear", allYear);				
+		
+		List<Task> findUserTasksYear = taskService.findUserTasksYear(user, year,sortP);
+		Page<Task> listPaged = (Page<Task>) taskService.findPaginated(PageRequest.of(currentPage /*-1*/ , pageSize, sortP /*Sort.by(sortParam)*/),findUserTasksYear);	
+//		PageWrapper<Task> pageWrapp = new PageWrapper<Task>(listPaged, "/profile");
+		model.addAttribute("tasks", listPaged);
+//		model.addAttribute("pageWrapp", pageWrapp);
+			
+		
+		session.setAttribute("currentPage", clikedPage  );
+		session.setAttribute("emailSession", email);
+		
+//		int totalPages = listPaged.getTotalPages();
+//        if (totalPages > 0) {
+//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+//                .boxed()
+//                .collect(Collectors.toList());
+//            model.addAttribute("pageNumbers", pageNumbers);
+//        }
+		return "views/profile";
+	
+	}
+	
+	@GetMapping("/profileYear")
+	public String showProfilePageByYears(
+			Model model,
+			@RequestParam(defaultValue="") String email,
+			@RequestParam("page") Optional<Integer> page,
+			@RequestParam("size") Optional<Integer> size,
+			@RequestParam("sort") Optional<String> sort,
+//			@RequestParam("selectedYear") Optional<String> selectedYear,
+			Principal principal,
+			HttpSession session,
+			HttpServletRequest request,
+			Pageable pageable
 			) {		
 		if(session.getAttribute("emailSession") != null) {
 			if(!session.getAttribute("emailSession").equals(email)) {
@@ -64,7 +120,7 @@ public class profileController {
 			clikedPage = currentPage;
 		}
 		Sort sortP = pageable.getSort();
-		Sort s = new Sort.Order(Direction.ASC, sort.orElse("aa"));
+		//Sort s = new Sort.Order(Direction.ASC, sort.orElse("aa"));
 		
 		
         int pageSize = /*pageable.getPageSize();*/ size.orElse(5);
@@ -86,47 +142,15 @@ public class profileController {
 		
 		session.setAttribute("currentPage", clikedPage  );
 		session.setAttribute("emailSession", email);
-		
-		int totalPages = listPaged.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                .boxed()
-                .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-		return "views/profile";
-	
-	}
-	
-	@GetMapping("/profileYear")
-	public String showProfilePageByYears(
-			Model model,
-			@RequestParam("selectedYear") String selectedYear,
-			@RequestParam(defaultValue="") String email,
-			HttpServletResponse response,
-			Principal principal,
-			@RequestParam("page") Optional<Integer> page,
-			@RequestParam("size") Optional<Integer> size,
-			@SortDefault("startDate") Pageable pageable
-			) {
-		int currentPage = page.orElse(1);
-        int pageSize = size.orElse(5);
-		if(email.isEmpty()) email = principal.getName();
-		User user = userService.findOne(email);
-		model.addAttribute("years", taskService.getAllYeas(user));
-		model.addAttribute("email",user.getEmail());		
-		List<Task> findUserTasksYear = taskService.findUserTasksYear(user, selectedYear);
-		Page<Task> listPaged = (Page<Task>) taskService.findPaginated(PageRequest.of(currentPage -1 , pageSize),findUserTasksYear);
-		model.addAttribute("tasks", listPaged);
 				
-		int totalPages = listPaged.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                .boxed()
-                .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-		
+//		int totalPages = listPaged.getTotalPages();
+//        if (totalPages > 0) {
+//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+//                .boxed()
+//                .collect(Collectors.toList());
+//            model.addAttribute("pageNumbers", pageNumbers);
+//        }
+//		
 		
 		return "views/profileTableData";
 	}
